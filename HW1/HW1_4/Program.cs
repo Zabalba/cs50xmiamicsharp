e895ef7b9437d07a2dcdb32698e026d78a8f1682
Zabalba <kevin.contino@gmail.com>
2016-02-04 22:45:29 -0500

    Addition of HW1_4

---
+++ HW1/HW1_4/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW1_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            /*
+            4. All types in C# derive from object. 
+            Create a Student class like we did this week. 
+            Add Name, Grade and Gender properties. 
+            Override the Equals function in your Student class to 
+            allow instances of Students to compare themselves for equality. 
+            Two students should be considered equal if the 
+            Name and Gender properties are the same.
+            */
+
+            Student s1 = new Student { Name = "Kevin", Grade = 4.0, Gender = "Male" };
+            //Student s2 = new Student { Name = "April", Grade = 4.2, Gender = "Female" };
+            Student s2 = new Student { Name = "Kevin", Grade = 4.0, Gender = "Male" };
+
+            Console.WriteLine(s1.Equals(s2).ToString());
+            Console.ReadKey();
+
+        }
+    }
+    class Student
+    {
+        public string Name { get; set; }
+        public double Grade { get; set; }
+        public string Gender { get; set; }
+
+        public override bool Equals(object obj)
+        {
+
+            if (obj == null || GetType() != obj.GetType())
+                return false;
+
+            Student inc = (Student)obj;
+            return (Name.Equals(inc.Name, StringComparison.OrdinalIgnoreCase)
+                    && Gender.Equals(inc.Gender, StringComparison.OrdinalIgnoreCase));
+        }
+    
+    }
+}
